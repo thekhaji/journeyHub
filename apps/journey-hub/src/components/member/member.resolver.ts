a63e2f6358824cdec61c308f641fd0ea8750cb4b
Khojiakbar <khojiakbar_kodirov@mail.ru>
2024-11-07 03:57:37 +0900

    feat: create member related resolver and service

---
+++ apps/journey-hub/src/components/member/member.resolver.ts
@@ -0,0 +1,12 @@
+import { Mutation, Resolver } from '@nestjs/graphql';
+import { MemberService } from './member.service';
+
+@Resolver()
+export class MemberResolver {
+    constructor(private readonly memberService: MemberService){}
+
+    @Mutation(()=>String)
+    public async signup(): Promise<string>{
+        
+    }
+}
