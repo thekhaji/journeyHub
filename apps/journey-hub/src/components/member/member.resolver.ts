eea5dca902444d8ada970df797e043b56d7ea3bd
Khojiakbar <khojiakbar_kodirov@mail.ru>
2024-11-12 09:50:25 +0900

    feat: develop globa GraphQL error handling system

--- apps/journey-hub/src/components/member/member.resolver.ts
+++ apps/journey-hub/src/components/member/member.resolver.ts
@@ -14,30 +14,15 @@ export class MemberResolver {
     @Mutation(()=>Member)
     
     public async signup(@Args("input") input: MemberInput): Promise<Member>{
-        try {
             console.log("Mutation: signup");
-
             return this.memberService.signup(input);
-        } catch (err) {
-            console.log("Error, signup:", err);
-            throw new InternalServerErrorException(err);
-            
-        }
-        
     }
 
     @Mutation(()=>Member)
     
     public async login(@Args("input") input: LoginInput): Promise<Member>{
-        try {
             console.log("Mutation: login");
-
             return this.memberService.login(input);
-        } catch (err) {
-            console.log("Error, login:", err);
-            throw new InternalServerErrorException(err);
-            
-        }
     }
 
     @Mutation(()=>String)
