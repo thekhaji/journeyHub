4e45282acd7e39de2424aa77f4edbe839fd5fce3
Khojiakbar <khojiakbar_kodirov@mail.ru>
2024-11-14 09:00:39 +0900

    feat: creation and verification jwt

--- apps/journey-hub/src/components/member/member.service.ts
+++ apps/journey-hub/src/components/member/member.service.ts
@@ -18,7 +18,7 @@ export class MemberService {
         input.memberPassword = await this.authService.hashPassword(input.memberPassword);
         try {
             const result = await this.memberModel.create(input);
-            //TODO Authentication via TOKEN
+            result.accessToken = await this.authService.createToken(result);
             return result;
         } catch (err) {
             console.log("Error, Service.model", err.message);
@@ -41,7 +41,7 @@ export class MemberService {
         const isMatch = await this.authService.comparePasswords(input.memberPassword, response.memberPassword);
         if(!isMatch)
             throw new InternalServerErrorException(Message.WRONG_PASSWORD);
-        
+        response.accessToken = await this.authService.createToken(response);
         return response;
     }
     
